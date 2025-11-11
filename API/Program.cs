dfe41a11ce8010ab0141e4066a3243b2db9292ff
Guillermina Salti <guillesalti@gmail.com>
2025-11-10 21:02:15 -0300

    intento 1 deploy

--- API/Program.cs
+++ API/Program.cs
@@ -188,6 +188,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
 app.UseAuthentication();
 
 app.UseAuthorization();
